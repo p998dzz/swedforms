1bc19107e719b286af434d019a21e7499ed7211b
IngaS358 <deisy95@gmail.com>
2016-02-18 01:09:29 +0200

    ideta confirmation. ir bandziau angular pritaikyti newRegistration

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -10,17 +10,17 @@ appControllers.controller('loginController', function($scope, $http, $rootScope,
            url: $rootScope.url+'/authenticate',
            data: { "email": email, "pass": password }
        }).then(function successCallback(response) {
-             if(response.data != "null")
-             {
-                $rootScope.user = response.data;
-                $window.location.href = '/home';
-             }else
-             {
-                alert("Neteisingas vartotojas arba slaptažodis");
-             }
-           }, function errorCallback(response) {
-                alert("Problemos su interneto ryšiu");
-           });
+                     if(response.data != "null")
+                     {
+                        $rootScope.user = response.data;
+                        $window.location.href = '/home';
+                     }else
+                     {
+                        alert("Neteisingas vartotojas arba slaptažodis");
+                     }
+                   }, function errorCallback(response) {
+                        alert("Problemos su interneto ryšiu");
+                   });
      }
 });
 
@@ -34,9 +34,39 @@ appControllers.controller('homeController', function($scope, $http, $rootScope, 
 });
 
 appControllers.controller('newRegistrationController', function($scope, $http, $rootScope, $window) {
+    $scope.register = function() {
+           var name = $( "#nameField" ).val();
+           var surname = $( "#surnameField" ).val();
+           var telNumber = $( "#telNumberField" ).val();
+           var email = $( "#emailField" ).val();
 
+           $http({
+               method: 'POST',
+             //  url: $rootScope.url+'/authenticate',  sitoj vietoj nelabai suprantu koki uml rasyt
+               data: { "name":name, "surname": surname, "telNumber":telNumber, "email": email}
+           }).then(function successCallback(response) {
+                         if(response.data != "null")
+                         {
+                            $rootScope.user = response.data;
+                            $window.location.href = '/home';  //siaip turetu eit i registration confirmation ir nezinau dar kaip ta dali nuo then keist reik
+                         }else
+                         {
+                            alert("Neteisingas vartotojas arba slaptažodis");
+                         }
+                       }, function errorCallback(response) {
+                            alert("Problemos su interneto ryšiu");
+                       });
+
+            //$window.location.href = '/home';
+         }
 });
 
 appControllers.controller('ContactUsController', function($scope, $http, $rootScope, $window) {
 
+});
+
+appControllers.controller('confirmationController', function($scope, $http, $rootScope, $window) {
+    $scope.conf = function() {
+        $window.location.href = '/#/home';
+  }
 });
