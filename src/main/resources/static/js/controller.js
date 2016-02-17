5ac36ebc2bc1f364e3e9af610163f1c4cba90729
p998hll <lapinas17@gmail.com>
2016-02-17 15:38:11 +0200

    ContactUs created but not finished

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -23,3 +23,27 @@ appControllers.controller('loginController', function($scope, $http, $rootScope,
            });
      }
 });
+
+appControllers.controller('contactUsController', function($scope, $http, $rootScope, $window) {
+    $rootScope.url = "http://localhost:8080";
+    $scope.login = function() {
+       var email = $( "#emailField" ).val();
+       var password = $( "#passwordField" ).val();
+       $http({
+           method: 'POST',
+           url: $rootScope.url+'/authenticate',
+           data: { "email": email, "pass": password }
+       }).then(function successCallback(response) {
+             if(response.data != "null")
+             {
+                $rootScope.user = response.data;
+                $window.location.href = '/home';
+             }else
+             {
+                alert("Neteisingas vartotojas arba slaptažodis");
+             }
+           }, function errorCallback(response) {
+                alert("Problemos su interneto ryšiu");
+           });
+     }
+});
