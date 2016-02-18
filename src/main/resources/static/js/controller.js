6ac9e8733eba2cea863a03ced2767d50a5d9f5d4
IngaS358 <deisy95@gmail.com>
2016-02-18 14:50:23 +0200

    paredaguota newRegistration ir contactUS

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -13,7 +13,7 @@ appControllers.controller('loginController', function($scope, $http, $rootScope,
                      if(response.data != "null")
                      {
                         $rootScope.user = response.data;
-                        $window.location.href = '/home';
+                        $window.location.href = '/#/home';
                      }else
                      {
                         alert("Neteisingas vartotojas arba slaptažodis");
@@ -34,21 +34,24 @@ appControllers.controller('homeController', function($scope, $http, $rootScope, 
 });
 
 appControllers.controller('newRegistrationController', function($scope, $http, $rootScope, $window) {
+    $
+
+
     $scope.register = function() {
            var name = $( "#nameField" ).val();
            var surname = $( "#surnameField" ).val();
            var telNumber = $( "#telNumberField" ).val();
            var email = $( "#emailField" ).val();
-
+           var time = $("#timeSelect").val();
+            $rootScope.regData = { "name":name, "surname": surname, "telNumber":telNumber, "email": email, "date": date, "bankSection": bankSection, "Topic": topic, "comments": comments };
            $http({
                method: 'POST',
-             //  url: $rootScope.url+'/authenticate',  sitoj vietoj nelabai suprantu koki uml rasyt
-               data: { "name":name, "surname": surname, "telNumber":telNumber, "email": email}
-           }).then(function successCallback(response) {
-                         if(response.data != "null")
+               url: $rootScope.url+'/checkRegistration',  sitoj vietoj nelabai suprantu koki uml rasyt
+               data: $rootScope.regData
+           }).then(function successCallback(response) { //nezinau dar kaip ta dali nuo then keist reik
+                         if(response.data == "OK")
                          {
-                            $rootScope.user = response.data;
-                            $window.location.href = '/home';  //siaip turetu eit i registration confirmation ir nezinau dar kaip ta dali nuo then keist reik
+                            $window.location.href = '/#/home';  //siaip turetu eit i registration confirmation
                          }else
                          {
                             alert("Neteisingas vartotojas arba slaptažodis");
@@ -62,7 +65,31 @@ appControllers.controller('newRegistrationController', function($scope, $http, $
 });
 
 appControllers.controller('ContactUsController', function($scope, $http, $rootScope, $window) {
+       $scope.contact = function() {
+               var name = $( "#nameField" ).val();
+               var lastname = $( "#lastnameField" ).val();
+               var telNumber = $( "#phoneField" ).val();
+               var email = $( "#emailField" ).val();
 
+               $http({
+                   method: 'POST',
+                 //  url: $rootScope.url+'/authenticate',  sitoj vietoj nelabai suprantu koki uml rasyt
+                   data: { "name":name, "lastname": lastname, "telNumber":telNumber, "email": email}
+               }).then(function successCallback(response) { //nezinau dar kaip ta dali nuo then keist reik
+                             if(response.data != "null")
+                             {
+                                $rootScope.user = response.data;
+                                $window.location.href = '/#/confirmation';
+                             }else
+                             {
+                                alert("Neteisingas vartotojas arba slaptažodis");
+                             }
+                           }, function errorCallback(response) {
+                                alert("Problemos su interneto ryšiu");
+                           });
+
+                //$window.location.href = '/home';
+             }
 });
 
 appControllers.controller('confirmationController', function($scope, $http, $rootScope, $window) {
