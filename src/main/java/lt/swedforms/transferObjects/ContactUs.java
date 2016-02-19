fc70384d26a3440a791ad2c16636909037b2bc90
p988dzz <domantas.zitkevicius@gmail.com>
2016-02-19 15:27:48 +0200

    webservisai ir transfer object prideti

---
+++ src/main/java/lt/swedforms/transferObjects/ContactUs.java
@@ -0,0 +1,12 @@
+package lt.swedforms.transferObjects;
+
+/**
+ * Created by Super on 2/19/2016.
+ */
+public class ContactUs {
+    private String user;
+
+    public String getUser() {
+        return user;
+    }
+}
